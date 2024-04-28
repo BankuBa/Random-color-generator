d154b47be51e0a90b2edf81755690b76c1d43819
BankuBa <paaekow04@gmail.com>
2024-04-28 12:03:55 +0000

    initial

---
+++ code.js
@@ -0,0 +1,15 @@
+function changeColor() {
+  const hex_numbers = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `A`, `B`, `C`, `D`, `F`];
+  
+  let hexCode = ``;
+  for (let i = 0; i < 6; i++) {
+    var random_index = Math.floor(Math.random() * hex_numbers.length); 
+    hexCode += hex_numbers[random_index];
+  }
+  
+  document.getElementById(`hex-code`).innerHTML =`#`+ hexCode
+  document.getElementById(`hex-code`).style.color = `white`
+  document.getElementsByTagName(`body`)[0].style.background = `#`+hexCode
+  
+console.log(hexCode);
+}
